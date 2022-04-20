83d676cad9aae89529fa11970fb1a72c484e1670
pulchroxloom <code@dantebarbieri.dev>
2022-04-19 19:15:14 -0500

    Update starter code (wip)

--- src/graph-algorithms.h
+++ src/graph-algorithms.h
@@ -1,32 +1,49 @@
 #pragma once
 
+#include <istream>
+#include <ostream>
+#include <sstream>
 #include <list>
+#include <string>
 #include "weighted-graph.hpp"
+
+// if the arrow is a box, change to the other line
+#define ARROW_SEPARATOR " \u2192 "
+// #define ARROW_SEPARATOR " -> "
 
 template <typename T>
 using vertex_type = typename WeightedGraph<T>::vertex_type;
-
 template <typename T>
 using weight_type = typename WeightedGraph<T>::weight_type;
-
 template <typename T>
 using adjacency_list = typename WeightedGraph<T>::adjacency_list;
 
 /**
- * @brief 
+ * @brief Dijkstra's Algorithm - https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm#Algorithm
  * 
- * @tparam T - Type of vertices in graph, usually int
- * @param graph - Graph to find Dijkstra's algorithm
- * @return std::list<typename WeightedGraph<T>::vertex_type> 
+ * @tparam T type of data stored by a vertex
+ * @param graph weighted, directed graph to find single-source shortest-path
+ * @param initial_node source node in graph for shortest path
+ * @param destination_node destination node in graph for shortest path
+ * @return std::list<vertex_type<T>> list of nodes along shortest path including initial_node and destination_node, empty if no path exists
  */
 template <typename T>
-std::list<vertex_type<T>> dijkstrasAlgorithm(const WeightedGraph<T> graph, vertex_type<T> initial_node) {
-    
+std::list<vertex_type<T>> dijkstrasAlgorithm(const WeightedGraph<T> graph, vertex_type<T> initial_node, vertex_type<T> destination_node) {
+    // TODO
+    return std::list<vertex_type<T>>();
 }
 
+/**
+ * @brief Returns a Topological Ordering of the Graph - https://en.wikipedia.org/wiki/Topological_sorting#Depth-first_search
+ * 
+ * @tparam T type of data stored by a vertex
+ * @param graph graph upon which to perform a topological ordering
+ * @return std::list<vertex_type<T>> list of nodes in a topological order, or an empty list if no such ordering exists
+ */
 template <typename T>
-std::list<typename WeightedGraph<T>::vertex_type> topologicalSort(const WeightedGraph<T> graph) {
-    
+std::list<vertex_type<T>> topologicalSort(const WeightedGraph<T> graph) {
+    // TODO
+    return std::list<vertex_type<T>>();
 }
 
 template <typename T>
@@ -38,10 +55,10 @@ std::ostream& operator<<(std::ostream& o, const WeightedGraph<T>& graph) {
         for (auto iit = list.begin(); iit != list.end(); ++iit) {
             const vertex_type<T>& destination = iit->first;
             const weight_type<T>& weight = iit->second;
-            o << '(' << destination << ", w: " << weight << ')';
+            o << destination << "(" << weight << ')';
             auto next_iter = iit;
             if (++next_iter != list.end()) {
-                o << ", ";
+                o << ARROW_SEPARATOR;
             }
         }
         auto next_iter = it;
@@ -49,4 +66,29 @@ std::ostream& operator<<(std::ostream& o, const WeightedGraph<T>& graph) {
             o << "\n";
         }
     }
+    return o;
+}
+
+template <typename T>
+std::istream& operator>>(std::istream& i, WeightedGraph<T>& graph) {
+    using value_type = typename WeightedGraph<T>::value_type;
+
+    std::string line;
+    while (std::getline(i, line)) {
+        if (line.empty()) continue;
+        std::istringstream line_stream(line);
+        value_type vertex;
+        line_stream >> vertex;
+        if (line_stream.fail()) {
+            i.setf(line_stream.flags());
+            break;
+        }
+        graph.push_vertex(vertex);
+        value_type edge_end;
+        weight_type<T> edge_weight;
+        while (line_stream >> edge_end >> edge_weight) {
+            graph.push_edge(vertex, edge_end, edge_weight);
+        }
+    }
+    return i;
 }
