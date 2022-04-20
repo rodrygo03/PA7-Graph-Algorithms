f0e603b3f249445552a94bec572268aab12fa799
pulchroxloom <code@dantebarbieri.dev>
2022-04-19 22:58:19 -0500

    Update finished starter code

--- src/graph-algorithms.h
+++ src/graph-algorithms.h
@@ -3,69 +3,90 @@
 #include <istream>
 #include <ostream>
 #include <sstream>
+#include <limits>
 #include <list>
+#include <queue>
 #include <string>
+#include <unordered_set>
+#include <unordered_map>
+
 #include "weighted-graph.hpp"
+
+#include "graph-types.h"
 
 // if the arrow is a box, change to the other line
 #define ARROW_SEPARATOR " \u2192 "
 // #define ARROW_SEPARATOR " -> "
 
-template <typename T>
-using value_type = typename WeightedGraph<T>::value_type;
-
-template <typename T>
-using vertex_type = typename WeightedGraph<T>::vertex_type;
-template <typename T>
-using weight_type = typename WeightedGraph<T>::weight_type;
-template <typename T>
-using adjacency_list = typename WeightedGraph<T>::adjacency_list;
+// contains initializeSingleSource, relax, & updateHeap as well as the DijkstraComaparator
+#include "dijkstras-helpers.h"
 
 /**
- * @brief Dijkstra's Algorithm - https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm#Algorithm
- * 
+ * @brief Dijkstra's Algorithm - https://canvas.tamu.edu/courses/136654/files/35930572/preview Slide 16
+ *
  * @tparam T type of data stored by a vertex
  * @param graph weighted, directed graph to find single-source shortest-path
  * @param initial_node source node in graph for shortest path
  * @param destination_node destination node in graph for shortest path
- * @return std::list<vertex_type<T>> list of nodes along shortest path including initial_node and destination_node, empty if no path exists
+ * @return std::list<value_type<T>> list of nodes along shortest path including initial_node and destination_node, empty if no path exists
  */
 template <typename T>
-std::list<vertex_type<T>> dijkstrasAlgorithm(const WeightedGraph<T> graph, vertex_type<T> initial_node, vertex_type<T> destination_node) {
-    // TODO
-    return std::list<vertex_type<T>>();
+std::list<value_type<T>> dijkstrasAlgorithm(const WeightedGraph<T>& graph, vertex_type<T> initial_node, vertex_type<T> destination_node)
+{
+    std::unordered_map<value_type<T>, weight_type<T>> distances;
+    std::unordered_map<value_type<T>, std::optional<value_type<T>>> predecessors;
+    std::unordered_set<value_type<T>> s;
+    std::priority_queue<value_type<T>, std::vector<value_type<T>>, DijkstraComparator<T>> q(DijkstraComparator<T>{distances});
+
+    // TODO implement Dijkstra's Algorithm
+
+    // TODO create list by walking backwards through predecessors from the end
+
+    return std::list<value_type<T>>();
 }
+
+#include "top-sort-helpers.h"
 
 /**
  * @brief Returns a Topological Ordering of the Graph - https://en.wikipedia.org/wiki/Topological_sorting#Depth-first_search
- * 
+ *
  * @tparam T type of data stored by a vertex
  * @param graph graph upon which to perform a topological ordering
- * @return std::list<vertex_type<T>> list of nodes in a topological order, or an empty list if no such ordering exists
+ * @return std::list<value_type<T>> list of nodes in a topological order, or an empty list if no such ordering exists
  */
 template <typename T>
-std::list<vertex_type<T>> topologicalSort(const WeightedGraph<T> graph) {
+std::list<value_type<T>> topologicalSort(const WeightedGraph<T>& graph)
+{
+    std::unordered_map<value_type<T>, int> indegrees;
+    std::unordered_map<value_type<T>, int> topological_numbers;
+
     // TODO
-    return std::list<vertex_type<T>>();
+
+    return ::list<value_type<T>>();
 }
 
 template <typename T>
-std::ostream& operator<<(std::ostream& o, const WeightedGraph<T>& graph) {
-    for (auto it = graph.begin(); it != graph.end(); ++it) {
-        const vertex_type<T>& vertex = it->first;
-        const adjacency_list<T>& list = it->second;
+std::ostream &operator<<(std::ostream &o, const WeightedGraph<T> &graph)
+{
+    for (auto it = graph.begin(); it != graph.end(); ++it)
+    {
+        const vertex_type<T> &vertex = it->first;
+        const adjacency_list<T> &list = it->second;
         o << vertex << ": ";
-        for (auto iit = list.begin(); iit != list.end(); ++iit) {
-            const vertex_type<T>& destination = iit->first;
-            const weight_type<T>& weight = iit->second;
+        for (auto iit = list.begin(); iit != list.end(); ++iit)
+        {
+            const vertex_type<T> &destination = iit->first;
+            const weight_type<T> &weight = iit->second;
             o << destination << "(" << weight << ')';
             auto next_iter = iit;
-            if (++next_iter != list.end()) {
+            if (++next_iter != list.end())
+            {
                 o << ARROW_SEPARATOR;
             }
         }
         auto next_iter = it;
-        if (++next_iter != graph.end()) {
+        if (++next_iter != graph.end())
+        {
             o << "\n";
         }
     }
@@ -73,30 +94,36 @@ std::ostream& operator<<(std::ostream& o, const WeightedGraph<T>& graph) {
 }
 
 template <typename T>
-std::istream& readEdge(std::istream& i, value_type<T>& vertex, weight_type<T>& weight) {
+std::istream &readEdge(std::istream &i, value_type<T> &vertex, weight_type<T> &weight)
+{
     std::string s_vertex, s_weight;
     std::getline(i, s_vertex, '(');
     std::getline(i, s_weight, ')');
     std::istringstream stream(s_vertex + " " + s_weight);
     stream >> vertex >> weight;
-    if (stream.fail()) {
+    if (stream.fail())
+    {
         i.setf(stream.flags());
     }
     return i;
 }
 
 template <typename T>
-std::istream& operator>>(std::istream& i, WeightedGraph<T>& graph) {
+std::istream &operator>>(std::istream &i, WeightedGraph<T> &graph)
+{
     std::string line;
-    while (std::getline(i, line)) {
-        if (line.empty()) continue;
+    while (std::getline(i, line))
+    {
+        if (line.empty())
+            continue;
         std::istringstream line_stream(line);
         value_type<T> vertex;
         std::string s_vertex;
         std::getline(line_stream, s_vertex, ':');
         std::istringstream stream_vertex(s_vertex);
         stream_vertex >> vertex;
-        if (stream_vertex.fail()) {
+        if (stream_vertex.fail())
+        {
             i.setf(stream_vertex.flags());
             break;
         }
@@ -104,7 +131,8 @@ std::istream& operator>>(std::istream& i, WeightedGraph<T>& graph) {
         std::string separator;
         value_type<T> edge_end;
         weight_type<T> edge_weight;
-        while (readEdge<T>(line_stream, edge_end, edge_weight)) {
+        while (readEdge<T>(line_stream, edge_end, edge_weight))
+        {
             line_stream >> separator;
             graph.push_edge(vertex, edge_end, edge_weight);
         }
--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,8 @@
 "6: 5(5)\n" \
 "7: 6(6)"
 
+#define GRAPH_TYPE int
+
 template <typename T>
 void getGraph(WeightedGraph<T>& graph, int argc, char* argv[]) {
     if (argc < 2) {
@@ -22,11 +24,35 @@ void getGraph(WeightedGraph<T>& graph, int argc, char* argv[]) {
     }
 }
 
+template <typename T>
+std::ostream& printVertexList(std::ostream& o, std::list<T> list, std::string label) {
+    o << "Start " << label << ": ";
+    for (const auto& e : list) {
+        o << e;
+        if (&e != &list.back()) {
+            o << ARROW_SEPARATOR;
+        }
+    }
+    return o << " :End " << label;
+}
+
 int main(int argc, char* argv[]) {
-    WeightedGraph<int> graph; // most input files
+    WeightedGraph<GRAPH_TYPE> graph; // most input files
     // WeightedGraph<std::string> graph; // inputCourses.dat
 
     getGraph(graph, argc, argv);
 
     std::cout << graph << std::endl;
+
+    typename WeightedGraph<GRAPH_TYPE>::value_type start, end;
+    std::cout << "Input Start Vertex Label for Dijkstra's: ";
+    std::cin >> start;
+    std::cout << "Input End Vertex Label for Dijkstra's: ";
+    std::cin >> end;
+
+    std::list<typename WeightedGraph<GRAPH_TYPE>::value_type> shortest_path = dijkstrasAlgorithm(graph, start, end);
+    printVertexList(std::cout, shortest_path, "Dijkstra's") << std::endl;
+
+    std::list<typename WeightedGraph<GRAPH_TYPE>::value_type> topological_sort = topologicalSort(graph);
+    printVertexList(std::cout, topological_sort, "Topological Sort") << std::endl;
 }
