b9eff7c08bf86d421648e8d7f3ab31e4f8741b71
flu0r1ne <flu0r1ne@flu0r1ne.net>
2022-04-23 12:38:01 -0500

    Reformat, deduplicate

--- tests/assignment-utils/graph-assert.cpp
+++ tests/assignment-utils/graph-assert.cpp
@@ -1,6 +1,12 @@
 #include "graph-assert.h"
 #include "sample-graphs.h"
 
+static void print_graph_info(std::ostream & o, fs::path graph_path) {
+    o << "Graph: " << graph_path.filename() << std::endl;
+    o << "Located at: " << graph_path << std::endl;
+    o << std::endl;
+}
+
 std::ostream & _assert_shortest_paths_match(std::ostream & o, std::string const & type) {
     fs::path graph_path = get_graph_path();
 
@@ -13,9 +19,7 @@ std::ostream & _assert_shortest_paths_match(std::ostream & o, std::string const 
             std::list<uint> shortest_path = dijkstrasAlgorithm(graph, path.initial_node, path.destination_node);
 
             if(!path.contains_shortest_path(shortest_path)) {
-                o << "Graph: " << graph_path.filename() << std::endl;
-                o << "Path: " << graph_path << std::endl;
-                o << std::endl;
+                print_graph_info(o, graph_path);
 
                 o << "Shortest path from initial node " 
                   << path.initial_node
@@ -46,31 +50,40 @@ std::ostream & _assert_shortest_paths_match(std::ostream & o, std::string const 
 
 std::ostream & _assert_top_sort_order(std::ostream & o, std::string const & type) {
     fs::path graph_path = get_graph_path();
-    std::vector<fs::path> graph_paths = get_graphs(graph_path, "gnp_dag");
+    std::vector<fs::path> graph_paths = get_graphs(graph_path, type);
     
     for(auto const & graph_path : graph_paths) {
         WeightedGraph<uint> graph = read_graph(graph_path);
         std::unordered_set<uint> visited;
-        auto sorted_graph = topologicalSort(graph);
+        std::list<uint> sorted_graph = topologicalSort(graph);
+        
         if(sorted_graph.size() != graph.size()) {
+            print_graph_info(o, graph_path);
+
             o << "Number of vertices in graph and topological sort order do not match." << std::endl;
             o << "Expected: " << graph.size() << std::endl;
             o << "Actual: " << sorted_graph.size() << std::endl;
+
+            o << "Returned topological sort ordering:" << std::endl;
+            print_path<uint>(o, sorted_graph);
+            
             return o;
         }
-        for(auto vertex: sorted_graph) {
+
+        for(auto vertex : sorted_graph) {
             visited.insert(vertex);
-            for(auto adjacent_vertex_weight_pair: graph.at(vertex)) {
-                if(visited.end() != visited.find(adjacent_vertex_weight_pair.first)) {
-                    o << "Graph: " << graph_path.filename() << std::endl << std::endl;
-                    o << "Vertex: " << adjacent_vertex_weight_pair.first;
-                    o << " was found in adjacency list of vertex: " << vertex << ". ";
-                    o << adjacent_vertex_weight_pair.first << " should thus come first." << std::endl << std::endl;
+            for(auto const & [adj_vert, adj_weight]: graph.at(vertex)) {
+                (void) adj_weight;
+                
+                if(visited.end() != visited.find(adj_vert)) {
+                    print_graph_info(o, graph_path);
+
+                    o << "Vertex: " << adj_vert;
+                    o << " was found in adjacency list of vertex " << vertex << ". ";
+                    o << adj_vert << " should appear before " << vertex << " in the topological ordering." << std::endl << std::endl;
+                    
                     o << "Returned topological sort ordering:" << std::endl;
-                    for(auto vertex_again: sorted_graph) {
-                        o << vertex_again << ", ";
-                    }
-                    o << std::endl;
+                    print_path<uint>(o, sorted_graph);
 
                     return o;
                 }
