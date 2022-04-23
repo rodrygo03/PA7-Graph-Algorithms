23ea7f4d910801a83e12bcf1bc9f25c0b805e547
Carlos Alvarez del Castillo <carlosadelcs@tamu.edu>
2022-04-23 09:18:52 -0500

    Added top sort test

--- tests/tests/topological_sort.cpp
+++ tests/tests/topological_sort.cpp
@@ -3,9 +3,17 @@
 TEST(gnp_dag) {
     fs::path graph_path = get_graph_path();
     std::vector<fs::path> graph_paths = get_graphs(graph_path, "gnp_dag");
-
+    
     for(auto const & graph_path : graph_paths) {
         WeightedGraph<uint> graph = read_graph(graph_path);
-        // TODO
+        std::unordered_set<uint> visited;
+        auto sorted_graph = topologicalSort(graph);
+        for(auto vertex: sorted_graph) {
+            visited.insert(vertex);
+            for(auto adjacent_vertex_weight_pair: graph.at(vertex)) {
+                // for some reason ASSERT_EQ doesn't compile here
+                ASSERT_TRUE(visited.end() == visited.find(adjacent_vertex_weight_pair.first));
+            }
+        }
     }
 }
