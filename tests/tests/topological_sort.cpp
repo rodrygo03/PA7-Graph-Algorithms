f999379b680a4c293ee0001268ad7d64eebf3ae8
Carlos Alvarez del Castillo <carlosadelcs@tamu.edu>
2022-04-23 10:07:15 -0500

    test now provides output and checks for correct size
    hopefully I did this right

--- tests/tests/topological_sort.cpp
+++ tests/tests/topological_sort.cpp
@@ -1,19 +1,5 @@
 #include "executable.h"
 
-TEST(gnp_dag) {
-    fs::path graph_path = get_graph_path();
-    std::vector<fs::path> graph_paths = get_graphs(graph_path, "gnp_dag");
-    
-    for(auto const & graph_path : graph_paths) {
-        WeightedGraph<uint> graph = read_graph(graph_path);
-        std::unordered_set<uint> visited;
-        auto sorted_graph = topologicalSort(graph);
-        for(auto vertex: sorted_graph) {
-            visited.insert(vertex);
-            for(auto adjacent_vertex_weight_pair: graph.at(vertex)) {
-                // for some reason ASSERT_EQ doesn't compile here
-                ASSERT_TRUE(visited.end() == visited.find(adjacent_vertex_weight_pair.first));
-            }
-        }
-    }
+TEST(gnp_dag) { 
+    ASSERT_TOPOLOGICAL_ORDERING("gnp_dag"); 
 }
